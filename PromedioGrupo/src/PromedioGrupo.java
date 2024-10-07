2b2b91b156e4e73c4df985c4b8e22d0efaa5f11f
Ared <ared230000@gmail.com>
2024-10-07 12:02:10 -0600

    Se muestra información d ecada alumno en el arreglo

--- PromedioGrupo/src/PromedioGrupo.java
+++ PromedioGrupo/src/PromedioGrupo.java
@@ -41,8 +41,18 @@ public class PromedioGrupo {
         }
     }
 
+    public static void imprimirAlumnos(){
+        System.out.println("Contenido del arreglo alumnos: ");
+        for (Alumno a : alumnos){
+            System.out.println("--------------------");
+            System.out.println("Información del alumno: ");
+            System.out.println(a.toString());
+        }
+    };
+
     public static void main(String[] args) throws Exception {
         int n; //tamaño del grupo
+        double promedio; // El promedio del grupo
 
         // double[] calificaciones; //calificaciones de cada alumno
         // double suma = 0; //para sumar calificaciones
@@ -57,16 +67,11 @@ public class PromedioGrupo {
 
         // Construir el arreglo alumnos 
         alumnos = new Alumno[n];
+        // Introducir información de alumnos
+        leerAlumnos();
+        // Mostrar información del alumno
+        imprimirAlumnos();
 
-        // calificaciones = new double[n];//Construcción del arreglo
-
-        // Pedir cada calificación y acumularla
-        // for (int i = 0; i < calificaciones.length; i++){
-        //     System.out.println("Escribe la calificación del estudiante [" + (i+1) + "]:");
-        //     entrada = bufer.readLine();
-        //     calificaciones[i]= Double.parseDouble(entrada);
-        //     suma += calificaciones[i];
-        // }
         //Obtener el promedio
         // promedio = suma / n;
 
